3d2cdad45afc953c5f67aebb3a92a24df889bdf9
MathewBr <matvei_berezin@mail.ru>
2022-12-01 21:51:47 +0300

    Delete config_smtp.php

--- config/config_smtp.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'smtp_host' => 'smtp.gmail.com',
-    'smtp_port' => '465',
-    'smtp_protocol' => 'ssl',
-    'smtp_login' => 'matvei7berezin@gmail.com',
-    'smtp_password' => 'xkomfqdbefyuuqgm',
-];
